ef4fa8b133ce94c6b4026302a1ce00f9244a5ff7
Sherzod Mamadaliev <egnaf@yahoo.com>
2019-11-18 01:31:23 +0300

    Add findByEmail and findAllByOrderByEmailDesc methods to UserRepository

--- spring-boot-security-simple/src/main/java/com/company/app/repository/UserRepository.java
+++ spring-boot-security-simple/src/main/java/com/company/app/repository/UserRepository.java
@@ -4,6 +4,13 @@ import com.company.app.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Collection<User> findAllByOrderByEmailDesc();
 }
