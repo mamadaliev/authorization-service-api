16934e6c8c758597316e415984bc92f19c6ff1f1
Sherzod Mamadaliev <egnaf@yahoo.com>
2019-11-18 19:59:05 +0300

    Merge branch 'egnaf' into 'dev' #2
    * Add simple Spring Boot + Spring Security application #1
    
    Merge 'dev' into 'master'
    
    * Add readme file to display information about the project
    
    * Change application name from 'security-simple' to 'app'
    
    * Fix title of readme file
    
    * Add spring data jpa and User entity
    
    * Add roles as enumeration type
    
    * Add repository interface for user entity
    
    * Add user service and implementation of user service
    
    * Add spring-boot-starter dependency
    
    * Add findByEmail and findAllByOrderByEmailDesc methods to UserRepository
    
    * Add UserCreateForm and UserCreateFormValidator classes
    
    * Add UserCreateFormValidator class again
    
    * Add NotFoundException and EmailNotFoundException classes
    
    * Add spring boot security basic application

---
+++ spring-boot-security-simple/src/main/java/com/company/app/domain/form/UserCreateForm.java
@@ -0,0 +1,53 @@
+package com.company.app.domain.form;
+
+import com.company.app.domain.Role;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class UserCreateForm {
+
+    @NotEmpty
+    private String email = "";
+
+    @NotEmpty
+    private String password = "";
+
+    @NotEmpty
+    private String passwordRepeat = "";
+
+    @NotNull
+    private Role role = Role.USER;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordRepeat() {
+        return passwordRepeat;
+    }
+
+    public void setPasswordRepeat(String passwordRepeat) {
+        this.passwordRepeat = passwordRepeat;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
