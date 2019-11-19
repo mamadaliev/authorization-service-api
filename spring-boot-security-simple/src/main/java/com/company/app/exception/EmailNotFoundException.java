9e0491d43b4194a40464fb19c587c73fdfd87960
Sherzod Mamadaliev <egnaf@yahoo.com>
2019-11-19 11:38:03 +0300

    Fix project structure

--- spring-boot-security-simple/src/main/java/com/company/app/exception/EmailNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.company.app.exception;
-
-public class EmailNotFoundException extends NotFoundException {
-
-    public EmailNotFoundException(String email) {
-        super("The email '" + email + "' is not found Exception");
-    }
-}
