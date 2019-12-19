677ae23e0d6ca4bef64cc35fbe343096d66357f3
egnaf <egnaf@yahoo.com>
2019-12-19 04:01:29 +0300

    Add base entity class with create, update times and status properties

---
+++ src/main/java/com/github/egnaf/sbaj/model/Base.java
@@ -0,0 +1,29 @@
+package com.github.egnaf.sbaj.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+class Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+    @CreatedDate
+    @Column(name = "created")
+    private Date created;
+
+    @LastModifiedDate
+    @Column(name = "updated")
+    private Date updated;
+}
