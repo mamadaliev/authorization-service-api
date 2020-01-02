76d12775e4f728996bfc678cdd27c4ea06bed846
egnaf <egnaf@yahoo.com>
2020-01-02 12:55:42 +0300

    Fix package 'auth_api_service' to 'aas'

--- src/main/java/com/github/egnaf/auth_api_service/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.egnaf.auth_api_service.repositories;
-
-import com.github.egnaf.auth_api_service.models.RoleModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<RoleModel, Long> {
-}
