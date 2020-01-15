a3121a2b864f963234f8963494575cb75f4c4906
egnaf <egnaf@yahoo.com>
2020-01-16 02:15:07 +0300

    Change ComponentScan and EnableAutoConfiguration annotations to SpringBootAnnotation

--- src/main/java/com/github/egnaf/auth/Application.java
+++ src/main/java/com/github/egnaf/auth/Application.java
@@ -1,11 +1,9 @@
 package com.github.egnaf.auth;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan("com.github.egnaf.auth")
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
